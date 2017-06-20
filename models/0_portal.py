1218eaeb371d6051a6c8cddb30a1918ec7b7e45f
Mirek Zvolský <zvolsky@seznam.cz>
2017-06-20 16:36:41 +0200

    ppokus

--- models/0_portal.py
+++ models/0_portal.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 if 'mojeknihovna.eu' in request.env.http_host.lower():
-    redirect(URL('codex2020', 'default', 'index'))
+    pass#redirect(URL('codex2020', 'default', 'index'))
 elif 'sluzbicka.eu' in request.env.http_host.lower():
-    redirect(URL('sluzbicka', 'default', 'index'))
+    pass#redirect(URL('sluzbicka', 'default', 'index'))
