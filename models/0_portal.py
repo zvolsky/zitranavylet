e0df813f6e2e26a186093455a20503c313c66ace
Mirek Zvolský <zvolsky@seznam.cz>
2017-06-20 16:09:51 +0200

    pridana sluzbicka

--- models/0_portal.py
+++ models/0_portal.py
@@ -2,3 +2,5 @@
 
 if 'mojeknihovna.eu' in request.env.http_host.lower():
     redirect(URL('codex2020', 'default', 'index'))
+elif 'sluzbicka.eu' in request.env.http_host.lower():
+    redirect(URL('sluzbicka', 'default', 'index'))
